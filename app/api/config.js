fed395c40eb18054c652dcc59b609dd2da0309a1
demon3030313 <167988848+demon3030313@users.noreply.github.com>
2024-05-30 15:32:59 +0300

    Comment to commit

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,5 @@
 export const BASE_URL = 'https://hikkback.nomorepartiesco.ru/api';
+// export const BASE_URL = 'http://localhost:3001/api';
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
